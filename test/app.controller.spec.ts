5e98d0a9cb5d63b49c29986d306dcdab30ef9494
Sarah Rajab <sarah.rajab94@gmail.com>
2022-09-29 00:28:36 +0300

    update unit test

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -17,7 +17,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).to.be.eq('Hello World!');
+      expect(appController.getHello()).to.be.eq('Hello World!!');
     });
   });
 });
